3f86ff3abd7d25141bd8272e239f4f85744c0aad
silicondoo <silicondoo@gmail.com>
2020-02-09 18:06:37 +0900

    asdf

---
+++ iosProjects/ayu/ayu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ayu
+//
+//  Created by silicondoo on 2020/02/09.
+//  Copyright © 2020 silicondoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
