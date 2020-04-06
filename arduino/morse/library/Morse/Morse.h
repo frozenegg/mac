62dfa6b51c28d0149ed67ad262389b4ae59b1362
frozenegg <60460979+frozenegg@users.noreply.github.com>
2020-04-06 20:51:34 +0900

    Add files via upload

---
+++ arduino/morse/library/Morse/Morse.h
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+
+#ifndef Morse_h
+#define Morse_h
+
+class Morse
+{
+  public:
+    Morse(int pin);
+    void dot();
+    void dash();
+  private:
+    int _pin;
+};
+
+#endif
