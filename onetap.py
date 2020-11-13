fa82284fb5bcaace2c0caacc9c5540d4bf3f98a1
frozenegg <60460979+frozenegg@users.noreply.github.com>
2020-11-13 14:40:44 +0900

    Add files via upload

--- onetap.py
+++ onetap.py
@@ -1,14 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 import random
+import datetime
+from datetime import datetime, timedelta, date
+import json
+
+today = date.today()
+yesterday = today - timedelta(days=1)
+two_days_ago = today - timedelta(days=2)
+yesterday = str(yesterday)
+two_days_ago = str(two_days_ago)
 
 payload1 = {
     'player[email]': '',
     'player[password]': ''
 }
 
-temp_list = [35.8, 35.9, 36.0, 36.1, 36.2]
-body_temp = random.choice(temp_list)
+s = requests.Session()
+r = s.get('https://api.ouraring.com/v1/sleep?start={}&end{}&access_token=7JMT6BHEF3TI77K4M53ITWXXINLCV6SN'.format(two_days_ago, yesterday))
+json_data = r.json()
+temp_delta = json_data['sleep'][0]['temperature_delta']
+abs_temp = 36.0
+
+body_temp = abs_temp + temp_delta
 
 payload2 = {
     'condition_record[condition_item_values_attributes][0][condition_item_field_id]': 23941,
@@ -25,24 +39,21 @@ payload2 = {
     'condition_record[condition_item_values_attributes][4][check_values][]': 23970,
     'condition_record[condition_item_values_attributes][5][check_values][]': 23973,
     'condition_record[condition_item_values_attributes][6][check_values][]': 35439
-
 }
 
-s = requests.Session()
 r = s.get('https://www.one-tap.jp/players/sign_in')
-print(r.text)
-soup = BeautifulSoup(r.text, features="lxml")
+# print(r.text)
+soup = BeautifulSoup(r.text)
 auth_token = soup.find(attrs={'name': 'authenticity_token'}).get('value')
 payload1['authenticity_token'] = auth_token
-payload2['authenticity_token'] = auth_token
 
 r = s.post('https://www.one-tap.jp/players/sign_in', data=payload1)
-print(r.text)
+# print(r.text)
 soup = BeautifulSoup(r.text, features="lxml")
 auth_token = soup.find(attrs={'name': 'csrf-token'}).get('content')
 payload2['authenticity_token'] = auth_token
 r = s.get('https://www.one-tap.jp/teams/2873/player/condition_records/')
 # print(r.text)
 r = s.post('https://www.one-tap.jp/teams/2873/player/condition_records/', data=payload2)
-print(r.text)
-print(body_temp)
+# print(r.text)
+# print(body_temp)
